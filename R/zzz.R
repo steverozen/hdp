050fb88c0fdbded155f7e0a81952fc2621e1d914
Steven G. Rozen <steve@steverozen.net>
2020-05-29 13:10:56 +0800

    updated startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,8 @@
 .onAttach <- function(libname, pkgname){
-  packageStartupMessage("This is ", libname, " and ", pkgname)
+  uu <- sessionInfo()$otherPkgs
+  extra <- ifelse(is.null(uu), "", uu$hdpx$Version)
+  packageStartupMessage("This is ", libname, " and ", pkgname,
+                        " v", extra)
   # packageStartupMessage("Run citation('hdp') for citation instructions,
   #  and file.show(system.file('LICENSE', package='hdp')) for license details.")
 }
