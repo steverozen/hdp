98434fa8bc0c7c98f212d86944e8f66ff9fe2a03
Steven G. Rozen <steve@steverozen.net>
2020-05-29 12:48:55 +0800

    upgrading master from cdebug

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,8 @@
-.onAttach <- function(libname, pkgname){
-  packageStartupMessage("Run citation('hdp') for citation instructions,
-    and file.show(system.file('LICENSE', package='hdp')) for license details.")
+.onAttach <- function(libname, pkgname) {
+  uu <- sessionInfo()$otherPkgs
+  extra <- ifelse(is.null(uu), "", uu)
+  packageStartupMessage("This is ", libname, " and ", pkgname,
+                        " ", extra)
+  # packageStartupMessage("Run citation('hdp') for citation instructions,
+  #  and file.show(system.file('LICENSE', package='hdp')) for license details.")
 }
